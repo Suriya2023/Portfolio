b3973860f35c3a54b423fe3c2f26be9f5c299ab5
Suriya2023 <rajputsuraj221221@gmail.com>
2025-10-10 10:20:18 +0530

    recent comment

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.1f5pnrsqrt4"
+    "revision": "0.lhs9b6sggrg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
