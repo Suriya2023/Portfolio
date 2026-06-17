9d2b911ebba8288a5b359304441720c9fe603ff1
Suriya2023 <rajputsuraj221221@gmail.com>
2026-06-17 18:30:51 +0530

    final chanegs

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+
+if (import.meta.env.DEV && 'serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then((registrations) => {
+    registrations.forEach((registration) => registration.unregister());
+  });
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>   <App /></BrowserRouter>
-
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
